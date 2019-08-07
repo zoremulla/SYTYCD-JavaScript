3a77fbf4a70337e8dd376415a1824c1bb5974e41
MMulla <zore.mulla@gmail.com>
2019-08-07 19:39:04 +0300

    test commit

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@
  * - returns undefined if no matching book is found
  ****************************************************************/
 function getBookById(bookId, booksArray) {
-  return books.find(book => book.id === bookId);
+  return booksArray.find(book => book.id === bookId);
 }
 
 /**************************************************************
@@ -18,7 +18,7 @@ function getBookById(bookId, booksArray) {
  ****************************************************************/
 function getAuthorByName(authorName, authors) {
   return authors.find(
-    author => author.name.toLowercase() === authorName.toLowercase()
+    author => author.name.toLowerCase() === authorName.toLowerCase()
   );
 }
 
@@ -30,8 +30,8 @@ function getAuthorByName(authorName, authors) {
  ****************************************************************/
 function bookCountsByAuthor(authors) {
   return authors.map(auth => ({
-    author: author.name,
-    bookCount: author.books.length
+    author: auth.name,
+    bookCount: auth.books.length
   }));
 }
 
@@ -78,7 +78,7 @@ function titlesByAuthorName(authorName, authors, books) {
  * Note: assume there will never be a tie
  ****************************************************************/
 function mostProlificAuthor(authors) {
-  let prolificAuthor = authors[authors.length];
+  let prolificAuthor = authors[authors.length - 1];
 
   authors.forEach(author => {
     if (author.books.length > prolificAuthor.books.length) {
